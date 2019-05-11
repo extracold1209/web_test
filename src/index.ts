222415a9af0d4a6618ce19120e825ccc78afee31
extracold <extracold1209@gmail.com>
2019-05-11 15:16:56 +0900

    express route 분리

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-export default class calculator {
-    static Sum(a: number, b: number): number {
-      let c = a + b;
-      return c;
-    }
-  
-    static Difference(a: number, b: number): number {
-      let c = a - b;
-      return c;
-    }
-  }
+import express from './express';
+import http from 'http';
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(express);
+
+server.listen(port);
+server.on('error', (e) => {
+    console.error('http server listen error\n', e);
+});
+server.on('listening', () => {
+    console.log('server listen, PORT :', port);
+});
