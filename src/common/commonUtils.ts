c72d1fd1b1b6fe3e44387cffd0e7af965bf3c03c
extracold <extracold1209@gmail.com>
2019-05-22 12:31:35 +0900

    변수명 수정

--- src/common/commonUtils.ts
+++ src/common/commonUtils.ts
@@ -1,15 +1,15 @@
 type ObjectLike = {[key: string]: any};
 export default class {
     /**
-     * original 의 프로퍼티와 겹치는 source 프로퍼티를 주입한다.
+     * target 의 프로퍼티와 겹치는 source 프로퍼티를 주입한다.
      * 이때 value 는 source 을 따른다.
      * https://stackoverflow.com/a/49292614
      */
-    static projection = (original: ObjectLike, source: ObjectLike) => {
+    static projection = (target: ObjectLike, source: ObjectLike) => {
         return Object
             .keys(source)
             .reduce((result: ObjectLike, key: string) => {
-                if (source[key] && original[key]) {
+                if (source[key] && target[key]) {
                     result[key] = source[key];
                 }
                 return result;
